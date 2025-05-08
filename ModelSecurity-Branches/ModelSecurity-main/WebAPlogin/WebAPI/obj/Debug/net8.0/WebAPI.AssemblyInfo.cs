bbcff876494efdd3f8b126f670b1d03d549d2403
DanielBata <batahernandez12345@gmail.com>
2025-05-08 14:45:51 -0500

    CodigoLimpio

--- ModelSecurity-Branches/ModelSecurity-main/WebAPlogin/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ ModelSecurity-Branches/ModelSecurity-main/WebAPlogin/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e9ffa866e2a2887222c0a8fdd7a1c6b5477ed29")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
